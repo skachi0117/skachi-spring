f82465731d685d9f31305880d550f583ac33bece
skachi0117 <kasyunatts@gmail.com>
2024-06-21 13:13:20 +0900

    最初のコミット

---
+++ src/main/java/com/example/demo/entity/Test.java
@@ -0,0 +1,23 @@
+package com.example.demo.entity;
+
+import lombok.Data;
+
+@Data
+public class Test {
+	
+	private String name;
+	private int score;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getScore() {
+		return score;
+	}
+	public void setScore(int score) {
+		this.score = score;
+	}
+
+}
